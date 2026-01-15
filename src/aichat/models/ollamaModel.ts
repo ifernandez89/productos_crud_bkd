fba3d0034b7b45c115b699c36f9e464f7a27aada
Ignacio Gabriel Fernandez <nacho.xiphos@gmail.com>
2026-01-15 09:10:31 -0300

    nuevo modelo de prueba para programacion

--- src/aichat/models/ollamaModel.ts
+++ src/aichat/models/ollamaModel.ts
@@ -6,10 +6,21 @@ export class ModelService {
     public model: ChatOllama | null = null;;
 
     constructor() { }
-
+    /* 
+ollama pull qwen2.5-coder:14b
+Entrenado específicamente para código
+Muy fuerte en:
+TypeScript / JS
+Python
+Backend (NestJS, APIs)
+Refactors
+Explicaciones técnicas
+Corre muy bien con 32 GB RAM
+Es el mejor balance calidad tipo Opus / rendimiento local hoy.
+    */
     public async create() {
         this.model = new ChatOllama({//para 2 min
-            model: "openchat:7b",//gemma:2b phi llama3.2:1b(great for summarization) openchat:7b(comparaciones?)
+            model: "qwen2.5-coder:14b",//gemma:2b phi llama3.2:1b(great for summarization) openchat:7b(comparaciones?)
             temperature: 0.3,   // creatividad balanceada para naturalidad sin divagar
             topP: 0.9,         // limita un poco la aleatoriedad para coherencia
             topK: 20,           // suficiente para diversidad pero sin dispersarse
